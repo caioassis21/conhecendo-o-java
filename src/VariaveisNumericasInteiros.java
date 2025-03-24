9ff85f13f8adef341d594703ed69c7f66ee1de2c
Caio Assis <caioassisgyn@gmail.com>
2025-03-24 20:58:28 -0300

    Aula numeros inteiros em java

---
+++ src/VariaveisNumericasInteiros.java
@@ -0,0 +1,17 @@
+public class VariaveisNumericasInteiros {
+    public static void main(String[] args) {
+
+        int idade = 27;
+
+        System.out.println("A idade do Caio é " + idade);
+
+        // byte - 8 bits = -128 até 127
+        // short - 16 bits = -32768 até 32767
+        // int - 32 bits
+        // long - 64 bits
+
+        int testaExpressao = 10 * 5 + 50 + 24 / 2;
+
+        System.out.println("O resultado da expressão é " + testaExpressao);
+    }
+}
